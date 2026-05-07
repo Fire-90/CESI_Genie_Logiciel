991caf107d75843b3abc367192257bba52d83ca0
Mael <mael.prodhomme@gmail.com>
2026-05-07 12:24:21 +0200

    Dual Window

---
+++ EasySave/Views/ProcessTab.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Graphic.Views
+{
+    public partial class ProcessTab : UserControl
+    {
+        public ProcessTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
